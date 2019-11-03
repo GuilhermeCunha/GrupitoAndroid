ec73e8300f5490fef4f3bf63a5badf603c145fcd
GuilhermeCunha <guiscunha@gmail.com>
2019-11-02 22:30:27 -0300

    ListarUsuarios

--- GrupitoML/app/src/main/java/com/example/grupitoml/DAO/UsuarioDAO.java
+++ GrupitoML/app/src/main/java/com/example/grupitoml/DAO/UsuarioDAO.java
@@ -2,10 +2,14 @@ package com.example.grupitoml.DAO;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.example.grupitoml.DB.DBOpenHelper;
 import com.example.grupitoml.Model.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UsuarioDAO {
     public static boolean inserirUsuario(Usuario usuario, Context context){
@@ -26,4 +30,37 @@ public class UsuarioDAO {
         return resultado != -1;
 
     }
+    public static List<Usuario> listarUsuarios(Context context){
+        DBOpenHelper dbOpenHelper = new DBOpenHelper(context);
+        SQLiteDatabase db = dbOpenHelper.getReadableDatabase();
+
+        Cursor cursor;
+        String TABELA = "Usuarios";
+        String[] campos =  {"ID", "Email", "Nome", "Senha", "Telefone"};
+
+        cursor = db.query(TABELA, campos, null, null, null, null, null, null);
+
+        if(cursor!=null){
+            cursor.moveToFirst();
+            List<Usuario> usuarios = new ArrayList<>();
+            while (!cursor.isAfterLast()){
+
+                int ID = cursor.getInt(0);
+                String email = cursor.getString(1);
+                String nome = cursor.getString(2);
+                String senha = cursor.getString(3);
+                String telefone = cursor.getString(4);
+                Usuario us = new Usuario(ID, email, nome, senha, telefone);
+                usuarios.add(us);
+
+                cursor.moveToNext();
+            }
+            if (!cursor.isClosed()) {
+                cursor.close();
+            }
+            return usuarios;
+        }
+        return null;
+    }
 }
+
